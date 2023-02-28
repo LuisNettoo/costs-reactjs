5064345aa183d9592a1e27ff533a1b4487c58315
Luis Neto <116973054+LuisNettoo@users.noreply.github.com>
2023-02-27 23:35:11 -0300

    Função de acesso aos dados do banco

--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
@@ -6,6 +8,22 @@ import SubmitButton from "../form/SubmitButton";
 import styles from "./ProjectForm.module.css";
 
 const ProjectForm = ({ btnText }) => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <form className={styles.form} autoComplete="off">
       <Input
@@ -20,7 +38,11 @@ const ProjectForm = ({ btnText }) => {
         placeholder="Insira o orçamento total"
         name="budget"
       />
-      <Select name="category_id" text="Selecione a categoria" />
+      <Select
+        name="category_id"
+        text="Selecione a categoria"
+        options={categories}
+      />
       <SubmitButton text={btnText} />
     </form>
   );
