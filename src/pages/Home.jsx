f6bf2f1790d5aed11fc1273f2f74eca6397be96e
Luis Neto <116973054+LuisNettoo@users.noreply.github.com>
2023-02-27 22:35:03 -0300

    Page Home criada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
 import savings from "../assets/images/savings.svg";
-import "./Home.scss";
+import LinkButton from "../layout/LinkButton";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <section className="home__container">
+    <section className={styles.home_container}>
       <h1>
         Bem-vindo ao <span>Costs</span>
       </h1>
       <p>Comece a gerenciar seus projetos agora mesmo!</p>
-      <a href="/">Criar Projeto</a>
+      <LinkButton to="/newproject" text="Criar Projetos" />
       <img src={savings} alt="" />
     </section>
   );
