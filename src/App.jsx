a32a46258a9f17a94088d2258e35bd69e766a531
Luis Neto <santosneto.luis@gmail.com>
2023-03-05 18:00:20 -0300

    Route "project/:id" Created

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Projects from "./components/pages/Projects";
 import Container from "./components/layout/Container";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Project from "./components/pages/Project";
 
 const App = () => {
   return (
@@ -24,6 +25,7 @@ const App = () => {
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<Newproject />} />
+          <Route path="/project/:id" element={<Project />} />
         </Routes>
       </Container>
       <Footer />
